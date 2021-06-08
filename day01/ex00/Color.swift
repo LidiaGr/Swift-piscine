77e43f20ea3025dfe1d37507ea903f0172f815ab
Lidia Grigoreva <lidia.ls16@gmail.com>
2021-06-08 18:23:47 +0300

    added ex00

---
+++ day01/ex00/Color.swift
@@ -0,0 +1,8 @@
+public enum Color: String {
+    case clubs = "green"
+    case diamonds = "blue"
+    case hearts = "red"
+    case spades = "black"
+    
+    static let allColors : [Color] = [ clubs, diamonds, hearts, spades ]
+}
