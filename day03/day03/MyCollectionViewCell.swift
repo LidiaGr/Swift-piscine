bf2894c114e32c4f05e4c9488041760c5cb962dc
Lidia Grigoreva <lidia.ls16@gmail.com>
2021-06-19 13:17:47 +0300

    Added ex00

---
+++ day03/day03/MyCollectionViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  MyCollectionViewCell.swift
+//  day03
+//
+//  Created by Lidia Grigoreva on 19.06.2021.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    var tImage: UIImageView! = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .gray
+        return imageView
+    }()
+//    var spinner: UIActivityIndicatorView!
+    
+    
+    // MARK: - Public API
+    var imageURL: URL? {
+        didSet {
+            tImage?.image = nil
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        if let url = imageURL {
+//            spinner?.startAnimating()
+            DispatchQueue.global(qos: .utility).async {
+                let contentsOfURL = try? Data(contentsOf: url)
+                DispatchQueue.main.async {
+                    if url == self.imageURL {
+                        if let imageData = contentsOfURL {
+                            self.tImage?.image = UIImage(data: imageData)
+                        }
+//                        self.spinner?.stopAnimating()
+                    }
+                }
+            }
+        }
+    }
+    
+    
+//    private func updateUI() {
+////          print("fetching image")
+//          if let url = imageURL {
+//
+//              let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+//                  guard let data = data else {
+//                      return
+//                  }
+//                  // maybe try dispatch to main
+//                  DispatchQueue.main.async {
+//                      self.tImage.image = UIImage(data: data)
+//                  }
+//              }
+//              task.resume()
+//          }
+//      }
+
+}
+
+
---
+++ day03/day03/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  day03
+//
+//  Created by Lidia Grigoreva on 18.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var flag = Bool()
+    private var images = [UIImage]()
+    private var myCollectionView: UICollectionView
+    private let ImagesURI: [String] = [
+        "https://apod.nasa.gov/apod/image/2106/IMG_3915.jpg",
+        "https://www.nasa.gov/sites/default/files/iss036e009405-full.jpg",
+        "https://eoimages.gsfc.nasa.gov/images/imagerecords/148000/148447/shasta_oli_2019194_lrg.jpg",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/arp299.jpg"
+    ]
+    
+    init() {
+        myCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        myCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Images"
+        view.backgroundColor = .white
+        
+        myCollectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        
+        view.addSubview(myCollectionView)
+        myCollectionView.backgroundColor = .white
+        
+        myCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        myCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        myCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        myCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        myCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        myCollectionView.dataSource = self
+        myCollectionView.delegate = self
+        
+        
+        (myCollectionView.collectionViewLayout as! UICollectionViewFlowLayout).estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (myCollectionView.frame.width - 10) / 2, height: (myCollectionView.frame.width - 10) / 2)
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as! MyCollectionViewCell
+        
+        myCell.backgroundView = myCell.tImage
+        
+        let section = indexPath.section
+        let row = indexPath.row
+        let imageId = section + row + section
+        
+        myCell.imageURL = URL(string: ImagesURI[imageId])
+        
+        return myCell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("User tapped on item \(indexPath.section) \(indexPath.row)")
+    }
+}
+
