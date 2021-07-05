7dd5bc9385f0e4cf4e126a5d5f23c6ece0e7d812
Lidia Grigoreva <lidia.ls16@gmail.com>
2021-07-05 13:47:42 +0300

    day04 - fixed receiving Token

--- day04/day04/ViewController.swift
+++ day04/day04/ViewController.swift
@@ -10,13 +10,6 @@ import UIKit
 class ViewController: UIViewController {
     var apiController: APIController?
     var visitsArr: [Visit] = []
-    var token : String? {
-        willSet {
-            self.apiController = APIController(apiDelegate: self, apiToken: newValue!)
-            updateUI()
-        }
-    }
-    
     var tableView = UITableView()
     
     var search: UISearchController = {
@@ -36,6 +29,7 @@ class ViewController: UIViewController {
         return loginSpinner
     }()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -45,14 +39,13 @@ class ViewController: UIViewController {
         spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         spinner.startAnimating()
-        
-        getAccessToken()
     }
     
     func updateUI() {
         DispatchQueue.main.async { [self] in
             spinner.stopAnimating()
             self.title = "Station Sessions History"
+            self.apiController = APIController(apiDelegate: self, apiToken: Token!)
             
             search.searchBar.delegate = self
             search.delegate = self
@@ -138,44 +131,3 @@ extension ViewController: UISearchResultsUpdating, UISearchBarDelegate, UISearch
         self.tableView.reloadData()
     }
 }
-
-extension ViewController {
-    func getAccessToken() {
-        let UID = "be7059d0939c5aa48803d0c69f6372a85794ac39ab90814f338766f9e25c4f4a"
-        let SECRET = "27d4796f7fc88b986b6d0052a13560d3719950157344d21456a5bd5016a387ea"
-        let url = URL(string: "https://api.intra.42.fr/oauth/token".addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!)!
-        let data = ["grant_type":"client_credentials", "client_id":"\(UID)", "client_secret":"\(SECRET)"]
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = try? JSONSerialization.data(withJSONObject: data, options: [])
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let session = URLSession.shared
-        let dataTask = session.dataTask(with: request) { (data, response, error) in
-            if let err = error {
-                print(err)
-                return
-            }
-            guard let response = response as? HTTPURLResponse,
-                  (200...299).contains(response.statusCode) else {
-                print(response!)
-                return
-            }
-            if let recievedData = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: recievedData) as! Dictionary<String, AnyObject>
-                    guard let token = json["access_token"] as? String else { return }
-                    self.token = token
-                    print("Token successfully received")
-                } catch let err {
-                    print(err)
-                }
-            }
-        }
-        
-        dataTask.resume()
-    }
-}
-
-
