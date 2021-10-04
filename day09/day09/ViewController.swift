939929d4afe2cee06f8f76e5ead191eeac5d9fcd
Lidia Grigoreva <lidia.ls16@gmail.com>
2021-10-04 22:21:31 +0300

    added ex00 and ex01

--- day09/day09/ViewController.swift
+++ day09/day09/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import LocalAuthentication
+import ttarsha2021
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .red
+        authentication()
     }
 
+    private func authentication() {
+        let context = LAContext()
+
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+             let reason = "Identify yourself!"
+
+             context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) {
+                 [weak self] success, authenticationError in
+
+                 DispatchQueue.main.async {
+                     if success {
+                        print("success")
+                        self!.view.backgroundColor = .green
+                     } else {
+                        print("failure")
+                        self?.authentication()
+                         // error
+                     }
+                 }
+             }
+         } else {
+            print("enter password")
+             // no biometry
+         }
+    }
 
 }
-
